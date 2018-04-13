ddc8006466d0e36963e94648b120c17223f78392
Vlad <vladkozachek90@gmail.com>
2018-04-13 20:12:47 +0300

    Задание HW_1_1_loops (треугольник 1)

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,11 @@
 public class Main {
+    public static void main(String[] args) {
+        String [][]arr=new String[10][10];
+        for (int i=0;i<arr.length;i++){
+            for (int j=0;j<i;j++){
+                System.out.print(arr[i][j]=" * ");
+            }
+            System.out.println();
+        }
+    }
 }
