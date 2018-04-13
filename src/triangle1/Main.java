d51d58d695902e2adbd0caa3913a4487de940997
Vlad <vladkozachek90@gmail.com>
2018-04-13 20:33:20 +0300

    Задание HW_1_1_loops (треугольник_1)

---
+++ src/triangle1/Main.java
@@ -0,0 +1,20 @@
+package triangle1;
+
+public class Main {
+    public static void main(String[] args) {
+        String [][]arr=new String[10][10];
+        for (int i=0;i<arr.length;i++){
+            for (int j=0;j<i;j++){
+                System.out.print(arr[i][j]=" * ");
+            }
+            System.out.println();
+        }
+        for (int i=0;i<arr.length;i++){
+            for (int j=i;j<arr.length;j++){
+                System.out.print(arr[i][j]=" * ");
+            }
+            System.out.println();
+        }
+
+    }
+}
