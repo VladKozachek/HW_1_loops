6dee8cc7c3c7d35e4d27b61a204fbbbd6662d247
Vlad <vladkozachek90@gmail.com>
2018-04-16 20:01:51 +0300

    Задание HW_1_1_loops (треугольник_2)убрал ошибку

--- src/triangle2/Main.java
+++ src/triangle2/Main.java
@@ -3,7 +3,6 @@ package triangle2;
 public class Main {
     public static void main(String[] args) {
         String [][]arr=new String[10][10];
-        int temp=0;
         for (int i=0;i<arr.length;i++){
             for (int j=0;j<i;j++){
                 System.out.print(arr[i][j]="   ");
