615fa7faaf4af9f275a4a24d22a5a153ee38162f
Vlad <vladkozachek90@gmail.com>
2018-04-16 20:11:39 +0300

    Задание HW_1_1_loops (треугольник_3)

---
+++ src/triangle3/Main.java
@@ -0,0 +1,18 @@
+package triangle3;
+
+public class Main {
+    public static void main(String[] args) {
+        String [][]arr=new String[10][10];
+        for (int i=0;i<arr.length;i++){
+            for (int j=i;j<arr.length;j++){
+                System.out.print(arr[i][j]="   ");
+            }
+            for (int j=0;j<i+1;j++){
+                System.out.print(arr[i][j]=" * ");
+            }
+
+            System.out.println();
+        }
+
+    }
+}
